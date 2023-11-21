f1ca87a48be9244bc7e44d538e3f0ec79ac83f2e
Patrycja-dz <dziemianko.pat@gmail.com>
2023-11-21 08:26:14 +0100

    update example with more data

--- src/utils/initialValues.ts
+++ src/utils/initialValues.ts
@@ -3,30 +3,36 @@ export const initialValues = [
     id: 1,
     title: "test note 1",
     content: "bla bla note1",
+    priority: "high",
   },
   {
     id: 2,
     title: "test note 2 ",
     content: "bla bla note2",
+    priority: "high",
   },
   {
     id: 3,
     title: "test note 3",
     content: "bla bla note3",
+    priority: "high",
   },
   {
     id: 4,
     title: "test note 4 ",
     content: "bla bla note4",
+    priority: "high",
   },
   {
     id: 5,
     title: "test note 5",
     content: "bla bla note5",
+    priority: "high",
   },
   {
     id: 6,
     title: "test note 6",
     content: "bla bla note6",
+    priority: "high",
   },
 ];
